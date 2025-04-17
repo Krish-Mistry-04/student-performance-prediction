f33c294786985e0001f9be89f0fd67f836264a82
Krish-Mistry-04 <152404147+Krish-Mistry-04@users.noreply.github.com>
2025-04-17 18:57:42 +0530

    Model training

--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import sys
 import dill
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 def save_object(file_path,obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -12,14 +13,28 @@ def save_object(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models,params):
     try:
         report = {}
-        for model_name, model in models.items():
-            model.fit(X_train, y_train)
-            y_pred = model.predict(X_test)
-            score = r2_score(y_test, y_pred)
-            report[model_name] = score
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
+            y_train_pred = model.predict(X_train)
+
+            y_test_pred = model.predict(X_test)
+
+            train_model_score = r2_score(y_train, y_train_pred)
+
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
         return report
     
     except Exception as e:
